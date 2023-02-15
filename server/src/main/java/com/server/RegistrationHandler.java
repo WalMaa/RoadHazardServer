2c3c2b178aaed7405f6a9099564d0197ff277a28
WalMaa <wmaatta@gmail.com>
2023-02-15 17:42:38 +0200

    changed registrationhandler

--- server/src/main/java/com/server/RegistrationHandler.java
+++ server/src/main/java/com/server/RegistrationHandler.java
@@ -23,23 +23,29 @@ public class RegistrationHandler implements HttpHandler {
         if (exchange.getRequestMethod().equalsIgnoreCase("POST")) {
             InputStream stream = exchange.getRequestBody();
             String text = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8)).lines().collect(Collectors.joining("\n"));
+            if (text.startsWith(":")) {
+                exchange.sendResponseHeaders(405, -1);
+                return;
+            }
             String[] credentialArray = text.split(":");
-
-            //addUser checks whether the user exists is the credentialArray; returning false if it exists
+            
+            // addUser checks whether the user exists is the credentialArray; returning
+            // false if it exists
             if (userAuthenticator.addUser(credentialArray[0], credentialArray[1]) && (credentialArray[0].length() > 0) && credentialArray[1].length() > 0) {
                 exchange.sendResponseHeaders(200, -1);
+
             } else {
                 String responseString = "User already exists";
                 byte[] bytes = responseString.getBytes("UTF-8");
                 exchange.sendResponseHeaders(403, bytes.length);
-
+                
                 OutputStream outputStream = exchange.getResponseBody();
                 outputStream.write(responseString.getBytes());
                 outputStream.flush();
                 outputStream.close();
             }
             stream.close();
-
+            
         } else {
             String responseString = "Not supported";
             byte[] bytes = responseString.getBytes("UTF-8");
